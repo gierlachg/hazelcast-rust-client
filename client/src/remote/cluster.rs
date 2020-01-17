1b37ccdd0bafedb04ed6eef5f292c66d05324719
Grzegorz Gierlach <gierlachg@gmail.com>
2020-01-17 08:52:30 +0100

    Abstracting away collection of cluster members.

--- client/src/remote/cluster.rs
+++ client/src/remote/cluster.rs
@@ -7,24 +7,23 @@ use log::{error, info};
 
 use crate::{
     codec::Reader,
+    HazelcastClientError::ClusterNonOperational,
     message::{Message, Payload},
     // TODO: remove dependency to protocol ???
     protocol::Address,
     remote::member::Member,
-    HazelcastClientError::ClusterNonOperational,
     Result,
     TryFrom,
 };
 
 pub(crate) struct Cluster {
-    counter: AtomicUsize,
-    members: Vec<Member>,
+    members: Members,
 }
 
 impl Cluster {
     pub(crate) async fn connect<'a, E>(endpoints: E, username: &str, password: &str) -> Result<Self>
-    where
-        E: IntoIterator<Item = &'a str>,
+        where
+            E: IntoIterator<Item=&'a str>,
     {
         let mut members = vec![];
         for endpoint in endpoints {
@@ -38,36 +37,63 @@ impl Cluster {
         if members.is_empty() {
             Err(ClusterNonOperational)
         } else {
-            Ok(Cluster {
-                counter: AtomicUsize::new(0),
-                members,
-            })
+            Ok(Cluster { members: Members::new(members) })
         }
     }
 
     pub(crate) async fn dispatch<I, O>(&self, message: I) -> Result<O>
-    where
-        I: Into<Message>,
-        O: Payload + Reader,
+        where
+            I: Into<Message>,
+            O: Payload + Reader,
     {
-        let index = self.counter.fetch_add(1, Ordering::SeqCst);
-        match self.members[index % self.members.len()].send(message.into()).await {
-            Ok(message) => TryFrom::<O>::try_from(message),
-            Err(e) => Err(e),
+        match self.members.next() {
+            Some(member) => {
+                match member.send(message.into()).await {
+                    Ok(message) => TryFrom::<O>::try_from(message),
+                    Err(e) => Err(e),
+                }
+            }
+            None => Err(ClusterNonOperational)
         }
     }
 
     pub(crate) fn address(&self) -> &Option<Address> {
-        &self.members[0].address() // TODO: ???
+        &self.members.members[0].address() // TODO: !?!?!?
     }
 }
 
 impl fmt::Display for Cluster {
     fn fmt(&self, formatter: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(formatter, "\n\nMembers {{size: {}}} [\n", self.members.len(),)?;
+        write!(formatter, "\n\n{}\n", self.members)
+    }
+}
+
+struct Members {
+    sequence: AtomicUsize,
+    members: Vec<Member>,
+}
+
+impl Members {
+    fn new(members: Vec<Member>) -> Self {
+        Members { sequence: AtomicUsize::new(1), members }
+    }
+
+    fn next(&self) -> Option<&Member> {
+        if self.members.is_empty() {
+            None
+        } else {
+            let index = self.sequence.fetch_add(1, Ordering::SeqCst) % self.members.len();
+            Some(&self.members[index])
+        }
+    }
+}
+
+impl fmt::Display for Members {
+    fn fmt(&self, formatter: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(formatter, "Members {{size: {}}} [\n", self.members.len(), )?;
         for member in &self.members {
             write!(formatter, "\t{}\n", member)?;
         }
-        write!(formatter, "]\n")
+        write!(formatter, "]")
     }
 }
